9940d7c4c7741b90ecf920f7edfe07dab6c26faa
고영광 <70497430+readable-ko@users.noreply.github.com>
2022-07-08 11:05:56 +0900

    [Platinum III] Title: 돌 가져가기, Time: 64 ms, Memory: 8808 KB -BaekjoonHub

--- 백준/Platinum/22354. 돌 가져가기/돌 가져가기.cc
+++ 백준/Platinum/22354. 돌 가져가기/돌 가져가기.cc
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <math.h>
+#include <bits/stdc++.h>
 #define SETTING ios::sync_with_stdio(0), cin.tie(0)
 using namespace std ;
 int N, tmp ;
@@ -18,15 +14,19 @@ int main()
     cin >> tmp ;
     arr.push_back(tmp) ;
 
+    //같은 색 중복 제거하고 최대 값만 취하기
     for(int i = 1 ; i < chk.length() ; i++)
     {
         cin >> tmp ;
         if(chk[i-1] != chk[i]) arr.push_back(tmp) ;
         else if(tmp > arr.back()) arr.back() = tmp ;
     }
-    // 1:1 2:2 3:2 4:3 5:3   // 8 2 5 3
+    
+    // 결국 취하는 돌은 앞뒤를 제외한 것들 중 아래 갯수만큼임.
+    // 1:1 2:2 3:2 4:3 5:3
     int size = ceil((arr.size() - 2) / 2.0);
     priority_queue<int> pq ;
+    
     for(int i = 1 ; i < arr.size() - 1 ; i++)
         pq.push(arr[i]) ;
 
