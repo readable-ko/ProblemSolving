bff4f8b6d0e70e0bdab937ab369b0dd1c1cf882e
고영광 <70497430+readable-ko@users.noreply.github.com>
2022-07-08 10:22:55 +0900

    [Silver II] Title: 점프 점프, Time: 36 ms, Memory: 2396 KB -BaekjoonHub

---
+++ 백준/Silver/14248. 점프 점프/README.md
@@ -0,0 +1,12 @@
+# [Silver II] 점프 점프 - 14248 
+
+[문제 링크](https://www.acmicpc.net/problem/14248) 
+
+### 성능 요약
+
+메모리: 2396 KB, 시간: 36 ms
+
+### 분류
+
+너비 우선 탐색(bfs), 깊이 우선 탐색(dfs), 그래프 이론(graphs), 그래프 탐색(graph_traversal)
+
---
+++ 백준/Silver/14248. 점프 점프/점프 점프.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std ;
+int N, s, cnt ;
+
+int main()
+{
+    cin >> N ;
+    vector<int> arr(N) ;
+    vector<bool> visited(N) ;
+    queue<int> que ;
+
+    for(int& it : arr)
+        cin >> it ;
+    cin >> s ;
+
+    que.push(--s) ;
+    visited[s] = 1 ;
+
+    while(!que.empty())
+    {
+        int now = que.front() ;
+        que.pop() ;
+        cnt++ ;
+        
+        if(arr[now] + now < N && !visited[arr[now] + now])
+        {
+            visited[arr[now] + now] = 1 ;
+            que.push(arr[now] + now) ;
+        }
+        
+        if(now - arr[now] >= 0 && !visited[now - arr[now]])
+        {
+            visited[now - arr[now]] = 1 ;
+            que.push(now - arr[now]) ;
+        }
+    }
+
+    cout << cnt << "\n" ;
+}
