160703bbf885bb065e436768e27e2135944d1c5c
고영광 <70497430+readable-ko@users.noreply.github.com>
2022-11-01 00:02:14 +0900

    [Gold IV] Title: 연구소, Time: 384 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -3,14 +3,13 @@
  * N M이 몇인지도 나와있지 않아서 와 이거 어케푸냐.. 하고 있었는데
  * 그냥 감 1도 못잡다가 힌트 봤더니 브루트포스 문제 다시 보니까 8보다 작다고 명시되어있네
  * 브루트포스로 벽 위치를 다 찍어보고 그 순간마다 bfs 돌려봐야할 거 같은데. 시간 너무 오래걸리려나
- * dfs로 벽을 세우고 bfs돌리는 방법이 있었다!
+ * dfs로 벽 세우는 위치 찍고 bfs돌리는 방법이 있드라!
 **/
 
 #include <bits/stdc++.h>
 using namespace std;
 int N, M, tmp, global_max ;
 vector< vector<int> > arr;
-bool check[8][8] ;
 int x_plus[4] = {1, 0, -1, 0} ;
 int y_plus[4] = {0, 1, 0, -1} ;
 
