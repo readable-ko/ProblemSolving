9aa29a060955835b47b17049026fd85384caf05a
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-01-10 20:32:45 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,29 +1,47 @@
-/**
- * @file 1920
- * @author fpqpsxh
- * @brief 그냥 10만개 솔트 Nlog(N) 2번이면 오바되고
- *        하나 솔트하고 이분탐색하는 방법 귀찮아서 map으로 되나 짜봤다.
- * @date 2023-01-10
- */
-#include <bits/stdc++.h>
-using namespace std ;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(0), cin.tie(0) ;
-    int N, M, tmp ;
-    map<int, bool> mp ;
-    cin >> N ;
-    while(N--)
-    {
-        cin >> tmp ;
-        mp[tmp] = true ;
+int main(){
+    int n, m;
+    cin >> n;
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    vector<int>a(n);
+    int temp;
+    int ck;
+    int left, right, mid;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        a[i] = temp ;
+    }
+    sort(a.begin(),a.end());
+   
+
+    cin >> m;
+    for(int i = 0; i < m; i++){
+        cin >> temp;
+        ck = 0;
+        left = 0;
+        right = n - 1;
+        while(left <= right){
+            mid = (left + right)/2;
+            
+            if(temp < a[mid]){
+                right = mid - 1;
+            }
+            else if(temp > a[mid]){
+                left = mid + 1;
+            }
+            else{
+                ck = 1; 
+                break;
+            }
+        }
+        if(ck == 1){
+            cout << "1\n";
+        }
+        else cout << "0\n";
     }
 
-    cin >> M ;
-    while(M--)
-    {
-        cin >> tmp ;
-        cout << mp[tmp] << "\n" ;
-    }
 }
