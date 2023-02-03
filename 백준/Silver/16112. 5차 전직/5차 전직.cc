fd6a06d49f23b198b214290243897ffbefcba3f6
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-02-03 09:37:34 +0900

    [Silver II] Title: 5차 전직, Time: 60 ms, Memory: 3072 KB -BaekjoonHub

---
+++ 백준/Silver/16112. 5차 전직/5차 전직.cc
@@ -0,0 +1,29 @@
+/**
+ * @file 16112.cpp
+ * @author fpqpsxh
+ * @brief 그냥 보자마자 그리디더라
+ * @date 2023-02-02
+ */
+#include <bits/stdc++.h>
+using namespace std ;
+int N, K ;
+long long answer ;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0) ;
+    cin >> N >> K ;
+    int stone[N] ;
+    
+    for(int i = 0 ; i < N ; i++)
+    {
+        cin >> stone[i] ;
+    }
+    
+    sort(stone, stone + N) ;
+
+    for(int i = 0 ; i < N ; i++)
+        answer += (long long)stone[i] * min(i, K) ;
+
+    cout << answer << "\n";
+}
