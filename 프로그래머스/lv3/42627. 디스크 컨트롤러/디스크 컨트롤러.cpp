f7ead66df839f3eef323aca8801ae98261dc5135
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-04-14 11:34:15 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -10,28 +10,28 @@ struct compare
 };
 
 int solution(vector<vector<int>> jobs) {
-    int answer = 0, now = 0, idx= 0 ;
-    sort(jobs.begin(), jobs.end()) ;
+    int answer = 0, now = 0, idx= 0, N = jobs.size() ;
+    sort(jobs.begin(), jobs.end(), greater<>()) ;
     priority_queue<vector<int>, vector<vector<int>>, compare> pq ;
+    pq.push(jobs.back());
+    jobs.pop_back();
     
-    while(!pq.empty() || idx < jobs.size())
+    while(!pq.empty())
     {
-        if(idx < jobs.size() && now >= jobs[idx][0])
+        auto top = pq.top(); pq.pop();
+        
+        now = max(now + top[1], top[0] + top[1]) ;
+        answer += now - top[0] ;
+        
+        while(!jobs.empty() && jobs.back()[0] <= now)
         {
-            pq.push(jobs[idx++]);
-            continue ;
+            pq.push(jobs.back()); jobs.pop_back();
         }
         
-        if(!pq.empty())
+        if(pq.empty() && !jobs.empty())
         {
-            now += pq.top()[1] ;
-            answer += now - pq.top()[0] ;
-            pq.pop() ;
-        }
-        else
-        {
-            now = jobs[idx][0] ;
+            pq.push(jobs.back()); jobs.pop_back();
         }
     }
-    return answer / jobs.size() ;
+    return answer / N ;
 }
