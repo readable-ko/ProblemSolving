0df700ab0dd9b1c4f7d31d1a181a05a6ab6654ed
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-04-24 14:25:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
@@ -1,6 +1,6 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,18 +1,9 @@
 def solution(n, arr1, arr2):
     answer = []
     
-    for idx in range(len(arr1)):
-        bit_checker = (1 << n - 1)
-        num1 = arr1[idx]
-        num2 = arr2[idx]
-        l = ""
-        while bit_checker != 0:
-            if num1 & bit_checker == bit_checker:
-                l += "#"
-            elif num2 & bit_checker == bit_checker:
-                l += "#"
-            else:
-                l += ' '
-            bit_checker = bit_checker >> 1
-        answer.append(l)
+    for num1, num2 in zip(arr1, arr2):
+        s = bin(num1 | num2)[2:].rjust(n, '0')
+        s = s.replace('0', ' ')
+        s = s.replace('1', '#')
+        answer.append(s)
     return answer
