61a4eddd80b391f38e9821120da4661c0899024c
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-04-26 10:31:00 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.28 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,10 @@
+import re
+def solution(s):
+    answer = 0
+    mp = {'zero':'0', 'one':'1', 'two': '2', 'three':'3', 'four':'4',
+               'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    
+    for alphabet in mp.keys():
+        s = re.sub(alphabet, mp[alphabet], s)
+
+    return int(s)
