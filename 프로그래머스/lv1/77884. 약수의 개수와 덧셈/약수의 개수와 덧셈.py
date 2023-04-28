5abb066ada0cf770dbf2d6b02e9ba3d6652d2743
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-04-28 16:46:53 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,16 +1,8 @@
 def solution(left, right):
-    정답 = 0
-    약수 = [0] * 1001
-    반복 = max(left, right) + 1
-    
-    for i in range(1, 반복):
-        for j in range(i, 반복):
-            if j % i == 0:
-                약수[j] += 1
-    
-    for i in range(left, right+1):
-        if 약수[i] & 1 == 1:
-            정답 -= i
+    answer = 0
+    for i in range(left, right + 1):
+        if int(i**0.5) == i**0.5:
+            answer -= i
         else:
-            정답 += i
-    return 정답
+            answer += i
+    return answer
