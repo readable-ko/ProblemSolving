3c067d2970bd291f48a56ec517596bcd79242372
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-10-04 16:17:35 +0900

    [Gold IV] Title: ABBC, Time: 1876 ms, Memory: 139520 KB, Score: 57 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.py
+++ 백준/Gold/25381. ABBC/ABBC.py
@@ -9,11 +9,11 @@ def erase_b(target:str, give:str):
         if char == target:
             box.append(idx)
         
-        if char == give and len(box) > 0:
+        if char == give and len(box) > 0 and idx not in answer:
             answer.add(box.pop(0))
     
     return
 
-erase_b('A','B')
 erase_b('B','C')
+erase_b('A','B')
 print(len(answer))
