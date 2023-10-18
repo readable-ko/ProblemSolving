737c906ee529aab103af89b16ee7720d8df90739
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-10-18 12:59:15 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.07 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,62 +1,54 @@
 #include <string>
 #include <vector>
+#include <set>
+#include <iostream>
 #include <math.h>
-#include <queue>
+#include <algorithm>
+#include <numeric>
 using namespace std;
-/*
-* 근거. wire는 최대 100개 이하이며 각각을 기준으로 완전탐색은 최악 100회 이하
-* 10000번이므로 1초 이내이니까 완전탐색 때려보자!
-*/
 
-//sp: staring_point cp: cutting_point
-int BFS(vector< vector<int> > &tree, int sp, int cp) {
-    int cnt = 1 ;
-    queue<int> que ;
-    vector<bool> visited(101) ;
-    visited[sp] = 1 ;
+vector<int> parent ;
+vector<int> ranking ;
+int rank_max ;
+
+int Find(int num) {
+    if(parent[num] == num) return num ;
     
-    for(int branch : tree[sp])
-        if(!visited[branch] && branch != cp) {
-                visited[branch] = 1 ;
-                que.push(branch) ;
-                cnt++ ;
-            }
-    
-    while(!que.empty()) {
-        int front = que.front() ;
-        que.pop() ;
-        
-        for(int branch : tree[front])
-            if(!visited[branch]) {
-                visited[branch] = 1 ;
-                que.push(branch) ;
-                cnt++ ;
-            }
+    return Find(parent[num]) ;
+}
+
+void Union(int lhs, int rhs) {
+    int l = Find(lhs) ;
+    int r = Find(rhs) ;
+    if(l != r) {
+        if(ranking[l] >= ranking[r]) {
+            parent[r] = l ;
+            ranking[l] += ranking[r] ;
+            rank_max = ranking[l] ;
+        }
+        else {
+            parent[l] = r ;
+            ranking[r] += ranking[l] ;
+            rank_max = ranking[r] ;
+        }
     }
-    
-    return cnt ;
 }
 
 int solution(int n, vector<vector<int>> wires) {
-    int answer = 101;
-    // 1. 트리 완성하기
-    vector< vector<int> > tree(101) ;
+    int answer = 10e3;
     
-    for(vector<int> wire : wires) {
-        tree[wire[0]].push_back(wire[1]) ;
-        tree[wire[1]].push_back(wire[0]) ;
-    }
-    
-    
-    // 2. 각 wire를 기준으로 끊어내서 양옆 사이즈 구하기
-    // 3. 사이즈 차이 최소를 정답으로.
-    for(vector<int> wire : wires) {
-        int rhs = BFS(tree, wire[0], wire[1]) ;
-        int lhs = BFS(tree, wire[1], wire[0]) ;
+    for(int i = 0 ; i < wires.size() ; i++) {
+        parent = vector<int>(n + 1) ;
+        iota(parent.begin(), parent.end(), 0) ;
         
-        if(abs(rhs - lhs) < answer)
-            answer = abs(rhs - lhs) ;
+        ranking = vector<int>(n+1, 1) ;
+        rank_max = 1 ;
+        
+        for(int j = 0 ; j < wires.size() ; j++) if(i != j) {
+            Union(wires[j][0], wires[j][1]) ;
+        }
+
+        answer = min(answer, abs(2 * rank_max - n)) ;
     }
-    
     return answer;
 }
