807d7d6bec9041460daa29099396e79fa3bd95a9
고영광 <70497430+readable-ko@users.noreply.github.com>
2024-01-05 03:53:18 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,30 @@
 import sys
-
 N = int(sys.stdin.readline())
-connectLength = int(sys.stdin.readline())
-computers = [[] for i in range(N+1)]
-visited = [False for i in range(N+1)]
-visited[1] = True
+parents = [i for i in range(N+1)]
 
-def findVirus(node):
-    cnt = 0
-    for computer in computers[node]:
-        if not visited[computer]:
-            visited[computer] = True
-            cnt += findVirus(computer) + 1
-    return cnt
+def find(child):
+    if child == parents[child]:
+        return child
+    return find(parents[child])
 
-for connect in range(connectLength):
-    lhs, rhs = map(int, sys.stdin.readline().split())
-    computers[lhs].append(rhs)
-    computers[rhs].append(lhs)
+def union(lhs, rhs):
+    l = find(lhs)
+    r = find(rhs)
+    
+    if l == r:
+        return
+    elif l > r:
+        parents[l] = r
+    else:
+        parents[r] = l
 
-print(findVirus(1))
+netLength = int(sys.stdin.readline())
+for i in range(netLength):
+    lhs, rhs = map(int,sys.stdin.readline().split())
+    union(lhs, rhs)
+
+cnt = 0
+for item in parents:
+    if find(item) == 1:
+        cnt+=1
+print(cnt - 1)
