238cc87d8f8134dd65de4c7b5a967dde68f300c1
고영광 <70497430+readable-ko@users.noreply.github.com>
2024-02-15 02:36:35 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 676 ms, Memory: 37624 KB -BaekjoonHub

--- 백준/Gold IV/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold IV/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,5 +1,5 @@
 import sys
-from queue import Queue
+from collections import deque
 
 subin, sister = map(int, sys.stdin.readline().split())
 visited = [0x3f3f3f3f for i in range(100001)]
@@ -12,17 +12,17 @@ def check(depth, idx):
         return True
     return False
 
-q = Queue()
-q.put(subin)
+q = deque()
+q.append(subin)
 
-while not q.empty():
-    front = q.get()
+while len(q) > 0:
+    front = q.popleft()
 
     for item in [front - 1, front + 1, front * 2]:
         if check(front, item):
             if item == sister:
                 cnt += 1
-            q.put(item)
+            q.append(item)
 
 print(visited[sister])
 print(cnt)
