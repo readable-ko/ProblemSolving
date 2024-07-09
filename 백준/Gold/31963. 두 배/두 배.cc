344b1933ab651c954d7c742ef4890d64b277acaf
고영광 <70497430+readable-ko@users.noreply.github.com>
2024-07-09 17:10:44 +0900

    [Gold IV] Title: 두 배, Time: 36 ms, Memory: 4972 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.cc
+++ 백준/Gold/31963. 두 배/두 배.cc
@@ -2,27 +2,24 @@
 using namespace std;
 int N;
 
-int find_minimum_square(int target, int& now) {
-    int sp = 0;
-    while(target > now) {
-        sp++;
-        now *= 2;
-    }
-
-    return sp;
+int find_minimum_square(int x, int target, int now) {
+    return max(0, x + (int)ceil(log2((double)target/(double)now)));
 }
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
-    int prev = 0, answer = 0;
+    int prev = 0;
+    long long int answer = 0;
 
     cin >> N;
-    vector<int> sequence(N);
+    vector<long long int> sequence(N);
+    vector<int> power_cnt(N);
 
     for (int i = 0 ; i < N ; i++) {
         cin >> sequence[i];
-        if (i != 0 && sequence[i - 1] > sequence[i]) {
-            answer += find_minimum_square(sequence[i - 1], sequence[i]);
+        if (i != 0) {
+            power_cnt[i] += find_minimum_square(power_cnt[i - 1], sequence[i - 1], sequence[i]);
+            answer += power_cnt[i];
         }
     }
 
