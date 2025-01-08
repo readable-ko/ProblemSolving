af9d541af17e2d9ca2cd4abfebd153f967646ffb
고영광 <70497430+readable-ko@users.noreply.github.com>
2025-01-08 21:33:42 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 12 ms, Memory: 2264 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long int min_val=0, max_val=0, sp=0, answer;
+    cin >> min_val >> max_val;
+
+    answer= max_val - min_val + 1;
+    vector<bool> sieve = vector<bool>(max_val - min_val + 1, false);
+
+
+    for(long long int i = 2; i * i <= max_val; i++) {
+        
+        long long int ret = min_val / (i * i);
+
+        if(min_val % (i*i) != 0) {
+            ret++;
+        }
+        
+        while((i * i * ret) <= max_val) {
+            if(!sieve[(i*i)*ret - min_val]) {
+                sieve[(i*i)*ret - min_val] = true;
+                answer--;
+            }
+            ret++;
+        }
+    }
+
+    cout << answer;
+}
