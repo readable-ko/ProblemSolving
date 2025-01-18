425dbf2f86c88e9dd269e7f8091fb43dd6dd68f7
고영광 <70497430+readable-ko@users.noreply.github.com>
2025-01-18 21:13:14 +0900

    [Gold IV] Title: 팰린드롬?, Time: 228 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,45 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int checked[2001][2001];
-vector<int> num;
-
-bool checkPalindrom(int S, int E) {
-    if(S >= E) {
-        return true;
-    }
-
-    if(checked[S][E] == 0) {
-        if(num[S] != num[E]) {
-            checked[S][E] = -1;
-            return false;
-        }
-        bool result = checkPalindrom(S + 1, E - 1);
-
-        checked[S][E] = result ? 1 : -1;
-        return result;
-    } else if(checked[S][E] > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
+vector<int> arr;
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
+    
     int N;
     cin >> N;
-    num = vector<int>(N+1);
+    arr = vector<int>(N + 1);
 
-    for(int i = 1 ; i <= N ; i++) {
-        cin >> num[i];
-        checked[i][i] = 1;
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
     }
-    
-    int S, E, M;
+
+    for (int i = 1; i <= N; i++) {
+        checked[i][i] = 1;
+        if (i < N && arr[i] == arr[i + 1]) {
+            checked[i][i + 1] = 1;
+        }
+    }
+
+    for (int len = 3; len <= N; len++) {
+        for (int i = 1; i <= N - len + 1; i++) {
+            int j = i + len - 1;
+            if (arr[i] == arr[j] && checked[i + 1][j - 1] == 1) {
+                checked[i][j] = 1;
+            }
+        }
+    }
+
+    int M, S, E;
     cin >> M;
-    for(int i = 0 ; i < M; i++) {
+    while (M--) {
         cin >> S >> E;
-        checkPalindrom(S, E);
-        cout << (checked[S][E] > 0 ? 1 : 0) << "\n";
+        cout << checked[S][E] << "\n";
     }
 }
