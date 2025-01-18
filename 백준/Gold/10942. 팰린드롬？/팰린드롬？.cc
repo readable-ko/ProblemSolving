1abdcf158a9da739ec687b5f4667c0543b51326f
고영광 <70497430+readable-ko@users.noreply.github.com>
2025-01-18 21:10:09 +0900

    [Gold IV] Title: 팰린드롬?, Time: 236 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+int checked[2001][2001];
+vector<int> arr;
+
+bool checkPalindrom(int S, int E) {
+    if(S >= E) {
+        return true;
+    }
+
+    if(checked[S][E] == 0) {
+        if(arr[S] != arr[E]) {
+            checked[S][E] = -1;
+            return false;
+        }
+        bool result = checkPalindrom(S + 1, E - 1);
+
+        checked[S][E] = result ? 1 : -1;
+        return result;
+    } else if(checked[S][E] > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int N;
+    cin >> N;
+    arr = vector<int>(N+1);
+
+    for(int i = 1 ; i <= N ; i++) {
+        cin >> arr[i];
+        checked[i][i] = 1;
+    }
+    
+    int S, E, M;
+    cin >> M;
+    for(int i = 0 ; i < M; i++) {
+        cin >> S >> E;
+        checkPalindrom(S, E);
+        cout << (checked[S][E] > 0 ? 1 : 0) << "\n";
+    }
+}
